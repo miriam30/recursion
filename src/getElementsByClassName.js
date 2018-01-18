4d47a6c912006b2ca001046ca58db3352b3341b3
miriam30 <miriam.alvarez.p96@gmail.com>
2018-01-18 15:06:18 -0600

    fixed syntax

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,24 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+  var result = [],
+  body = document.body;
+  (function traverseDOM (node, fn) {
+    if (node.nodeType === 1) {
+      fn(node, className)
+    }
+    node = node.firstChild;
+    while (node) {
+      traverseDOM(node, fn);
+      node = node.nextSibling;
+    }
+  }
+  (body, function (elem, cName) {
+    if ((' ' + (elem.className || elem.getAttribute('class')) + ' ').indexOf(' ' + cName + ' ') >= 0) {
+      result.push(elem);
+    }
+  }));
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var array = [];
+  var newObj = {};
+  if(typeof obj[0] ==== "undefined" || typeof obj[0] === "symbol" || typeof obj[0] === "function"){
+    array.push(null)
+    return array.concat(stringifyJSON(obj.slice(obj[0])))
+  }
+  if(typeof obj === "number"||typeof obj === null || typeof obj=== "boolean"){
+    return obj + " " ;
+  }
+  else if(obj === "string"){
+    return obj;
+  }
+  else if(Array.isArray(obj)){
+    obj.forEach(stringify(element))
+  }
+  return '[' + arr + ']'
 };
+
+//
+/*var stringifyJSON = function(obj) {
+  // your code goes here
+
+if()
+Array.isArray(obj){
+return []
+}
+
+
+
+
+if(typeof obj[0] ==== "undefined" || typeof obj[0] === "symbol" || typeof obj[0] === "function"){
+return null
+}
+if(typeof obj[0] === "number"){
+  "'"+ obj[0]+ "'"
+}
+if(typeof obj[0] === "string"){
+
+}
+
+  arr.push("")
+    return arr.concat(stringifyJSON(obj.slice(obj[0])))*/
